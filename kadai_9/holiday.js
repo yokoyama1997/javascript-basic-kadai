9c50554b957c760ce788bf235814401593ebcde7
yokoyama1997 <154443346+yokoyama1997@users.noreply.github.com>
2024-02-17 00:30:48 +0900

    Add files via upload

---
+++ kadai_9/holiday.js
@@ -0,0 +1,14 @@
+const holidas = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","憲法記念日","みどりの日"
+                  ,"こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
+console.log(holidays);
+
+let holidays = Math.floor(Math.random() * 16);
+
+while(holidays !== 0) {
+    holidays = Math.floor(Math.random() * 16);
+}
+
+for (let i = 1; i <= 16; i++) {
+    console.log(holidays[i]);
+}
+            
