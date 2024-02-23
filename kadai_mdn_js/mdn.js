d5240ba4b3cef2fc0587055fc12ecc01550ab31f
yokoyama1997 <154443346+yokoyama1997@users.noreply.github.com>
2024-02-24 01:10:48 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,9 +0,0 @@
-const date = new Date(2023, 8, 22,);
-
-const [month, day, year] =[
-    date.getMonth(),
-    date.getDate(),
-    date.getFullYear(),
-]
-
-console.log(year + '年' + month + '月' + day + '日');
